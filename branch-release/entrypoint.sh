056c22663eb12becc05fe4a584905c1c9595b54d
labkey-tchad <tchad@labkey.com>
2020-05-21 17:03:10 -0700

    GitHub actions for release branching and merging

---
+++ branch-release/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "GITHUB_TOKEN is not defined." >&2
+  exit 1
+fi
+
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "GITHUB_WORKSPACE is not available." >&2
+  exit 1
+fi
+
+cd $GITHUB_WORKSPACE
+
+bash -c "/branch_release.sh $*"
---
+++ merge-release/merge_release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v hub)" ]; then
+  echo 'Error: GitHub command line tool is not installed.' >&2
+  exit 1
+fi
+
+TRIAGE_ALIAS='labkey-teamcity' # 'LabKey/Triage'
+
+# GITHUB_REPOSITORY=LabKey/platform
+# if [ -z $GITHUB_REPOSITORY ]; then
+# 	echo "GitHub repository not specified" >&2
+# 	exit 1
+# fi
+
+PR_NUMBER=$1
+MERGE_BRANCH=$2 # ff_19.3.11
+TARGET_BRANCH=$3 # release19.3
+if [ -z $TARGET_BRANCH ] || [ -z $MERGE_BRANCH ] || [ -z $PR_NUMBER ]; then
+	echo "PR info not specified" >&2
+	exit 1
+fi
+
+git config --global user.name "github-actions[bot]"
+git config --global user.email "teamcity@labkey.com"
+
+echo "Merge approved PR from $MERGE_BRANCH to $TARGET_BRANCH."
+git fetch --unshallow
+git checkout $TARGET_BRANCH
+git merge origin/$MERGE_BRANCH -m "Merge $MERGE_BRANCH to $TARGET_BRANCH" && git push || {
+	echo "Failed to merge!" >&2
+	hub api repos/{owner}/{repo}/issues/$PR_NUMBER/comments --raw-field 'body=@'$TRIAGE_ALIAS' __ERROR__ Automatic merge failed!'
+	exit 1
+}
