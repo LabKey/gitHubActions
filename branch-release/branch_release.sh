b36de2c9f270f9487341049b153c44610c905e21
labkey-tchad <tchad@labkey.com>
2020-07-15 11:26:59 -0700

    Don't reference teams

--- branch-release/branch_release.sh
+++ branch-release/branch_release.sh
@@ -8,8 +8,6 @@ fi
 REVIEWER1='labkey-tchad'
 REVIEWER2='labkey-klum'
 ASSIGNEE='labkey-teamcity'
-# Reference in PR comments. Default action token can't see teams. 
-TEAM='LabKey/releasers'
 
 if [ -z "$GITHUB_SHA" ]; then
 	echo "Commit hash not specified" >&2
@@ -74,7 +72,6 @@ else
 	if ! hub pull-request -f -h "$FF_BRANCH" -b "$RELEASE_BRANCH" -a "$ASSIGNEE" -r "$REVIEWER1" -r "$REVIEWER2" \
 		-m "Fast-forward for ${TAG}" \
 		-m "_Generated automatically._" \
-		-m "_(Attn: @${TEAM})_" \
 		-m "**Approve all matching PRs simultaneously.**" \
 		-m "**Approval will trigger automatic merge.**";
 	then
@@ -120,7 +117,6 @@ if git merge --no-ff "$GITHUB_SHA" -m "Merge ${TAG} to ${NEXT_RELEASE}"; then
 	if ! hub pull-request -f -h "$MERGE_BRANCH" -b "$TARGET_BRANCH" -a "$ASSIGNEE" -r "$REVIEWER1" -r "$REVIEWER2" \
 		-m "Merge ${TAG} to ${NEXT_RELEASE}" \
 		-m "_Generated automatically._" \
-		-m "_(Attn: @${TEAM})_" \
 		-m "**Approve all matching PRs simultaneously.**" \
 		-m "**Approval will trigger automatic merge.**";
 	then
@@ -143,7 +139,6 @@ else
 	if ! hub pull-request -f -h "$MERGE_BRANCH" -b "$TARGET_BRANCH" -a "$ASSIGNEE" -r "$REVIEWER1" -r "$REVIEWER2" \
 		-m "Merge ${TAG} to ${NEXT_RELEASE} (Conflicts)" \
 		-m "_Automatic merge failed!_ Please merge '${TARGET_BRANCH}' into '${MERGE_BRANCH}' and resolve conflicts manually." \
-		-m "_(Attn: @${TEAM})_" \
 		-m "**Approve all matching PRs simultaneously.**" \
 		-m "**Approval will trigger automatic merge.**";
 	then
