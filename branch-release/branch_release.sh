780fa5f6955ccce4d48f84e46abf8f743acab298
labkey-tchad <tchad@labkey.com>
2026-03-10 14:00:11 -0700

    Fix typo and update actions

--- branch-release/branch_release.sh
+++ branch-release/branch_release.sh
@@ -213,7 +213,7 @@ if ! hub api "repos/{owner}/{repo}/branches/${SNAPSHOT_BRANCH}"; then
 fi
 
 if [ "$(git rev-parse --is-shallow-repository)" = 'true' ] && ! git fetch --unshallow; then
-	echo "Failed to unshallow reposiotry. Rerun action." >&2
+	echo "Failed to unshallow repository. Rerun action." >&2
 	exit 1
 fi
 
